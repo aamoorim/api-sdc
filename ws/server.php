4d34bee881d8e48e6eff1dd8fcfee5abd198adeb
Mariana Amorim <amorimmariana007@gmail.com>
2025-10-23 16:21:11 -0300

    Remove websocket da develop para testes

--- ws/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ws/server.php
-declare(strict_types=1);
-
-require __DIR__ . '/../config/db.php'; // seu db.php retorna $pdo
-require __DIR__ . '/ChatServer.php';
-
-$server = new ChatServer('127.0.0.1', 9000, $pdo);
-$server->run();
